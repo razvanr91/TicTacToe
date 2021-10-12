b70f02cec9dcfb0db4ac4256700e43e3b0ba83ae
razvanr91 <razvan.roman91@outlook.com>
2021-10-12 16:19:10 +0100

    Add function for game start and remove listener after game ends

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ let gameBoardMatrix = [
 	["", "", ""],
 ];
 
+let playerWinFirstLineHorizontal =
+	gameBoardMatrix[0][0] === player &&
+	gameBoardMatrix[0][1] === player &&
+	gameBoardMatrix[0][2] === player;
+
+console.log(playerWinFirstLineHorizontal);
+
 // App elements
 let gameBoard = document.getElementById("gameBoard");
 let squares = gameBoard.querySelectorAll(".square");
 let resetButton = document.getElementById("reset");
 let alertDiv = document.getElementById("alertDiv");
+let currentPlayer = document.getElementById("currentPlayer");
 
 // GameBoard squares
 let oneOne = document.getElementById("oneOne");
@@ -26,96 +34,46 @@ let threeOne = document.getElementById("threeOne");
 let threeTwo = document.getElementById("threeTwo");
 let threeThree = document.getElementById("threeThree");
 
-resetButton.addEventListener("click", () => playAgain());
+resetButton.addEventListener("click", playAgain);
 
-gameBoard.addEventListener("click", (e) => {
-	if (e.target.classList.contains("square")) {
-		alertDiv.innerHTML = "";
-		addToken(e.target.id);
-		switchPlayer();
-	}
-});
+gameBoard.addEventListener("click", playGame);
 
 function addToken(id) {
 	switch (id) {
 		case "oneOne":
-			if (gameBoardMatrix[0][0] === "") {
-				gameBoardMatrix[0][0] = player;
-				oneOne.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
+			addToSquare(0, 0, oneOne);
 			break;
 		case "oneTwo":
-			if (gameBoardMatrix[0][1] === "") {
-				gameBoardMatrix[0][1] = player;
-				oneTwo.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
+			addToSquare(0, 1, oneTwo);
 			break;
 		case "oneThree":
-			if (gameBoardMatrix[0][2] === "") {
-				gameBoardMatrix[0][2] = player;
-				oneThree.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
-
+			addToSquare(0, 2, oneThree);
 			break;
 		case "twoOne":
-			if (gameBoardMatrix[1][0] === "") {
-				gameBoardMatrix[1][0] = player;
-				twoOne.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
-
+			addToSquare(1, 0, twoOne);
 			break;
 		case "twoTwo":
-			if (gameBoardMatrix[1][1] === "") {
-				gameBoardMatrix[1][1] = player;
-				twoTwo.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
+			addToSquare(1, 1, twoTwo);
 			break;
 		case "twoThree":
-			if (gameBoardMatrix[1][2] === "") {
-				gameBoardMatrix[1][2] = player;
-				twoThree.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
-
+			addToSquare(1, 2, twoThree);
 			break;
 		case "threeOne":
-			if (gameBoardMatrix[2][0] === "") {
-				gameBoardMatrix[2][0] = player;
-				threeOne.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
-
+			addToSquare(2, 0, threeOne);
 			break;
 		case "threeTwo":
-			if (gameBoardMatrix[2][1] === "") {
-				gameBoardMatrix[2][1] = player;
-				threeTwo.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
-
+			addToSquare(2, 1, threeTwo);
 			break;
 		case "threeThree":
-			if (gameBoardMatrix[2][2] === "") {
-				gameBoardMatrix[2][2] = player;
-				threeThree.innerHTML = player;
-			} else {
-				generateAlert("danger", "Choose other square. This one is already taken!");
-			}
-
+			addToSquare(2, 2, threeThree);
 			break;
+	}
+}
+
+function playGame(e) {
+	if (e.target.classList.contains("square")) {
+		alertDiv.innerHTML = "";
+		addToken(e.target.id);
 	}
 }
 
@@ -130,6 +88,63 @@ function generateAlert(type, message) {
 	alertDiv.append(alert);
 }
 
+function checkWinHorizontal(row) {
+	return (
+		gameBoardMatrix[row][0] === player &&
+		gameBoardMatrix[row][1] === player &&
+		gameBoardMatrix[row][2] === player
+	);
+}
+
+function checkWinVertical(column) {
+	return (
+		gameBoardMatrix[0][column] === player &&
+		gameBoardMatrix[1][column] === player &&
+		gameBoardMatrix[2][column] === player
+	);
+}
+
+function checkWinDiagonal() {
+	return (
+		(gameBoardMatrix[0][2] === player &&
+			gameBoardMatrix[1][1] === player &&
+			gameBoardMatrix[2][0] === player) ||
+		(gameBoardMatrix[0][0] === player &&
+			gameBoardMatrix[1][1] === player &&
+			gameBoardMatrix[2][2] === player)
+	);
+}
+
+function addToSquare(row, column, element) {
+	if (gameBoardMatrix[row][column] === "") {
+		gameBoardMatrix[row][column] = player;
+		element.innerHTML = gameBoardMatrix[row][column];
+		totalMoves++;
+		if (totalMoves >= 5) {
+			let winHorizontal = checkWinHorizontal(row) ? endGame() : "";
+			let winVertical = checkWinVertical(column) ? endGame() : "";
+			let winDiagonal = checkWinDiagonal() ? endGame() : "";
+		} else if (totalMoves === 9) {
+			player = "Nobody";
+			endGame();
+		}
+		switchPlayer();
+	} else {
+		generateAlert("danger", "Choose other square. This one is already taken!");
+	}
+}
+
+function endGame() {
+	if (totalMoves === 9) {
+		generateAlert("warning", "Nobody won.");
+		currentPlayer.innerHTML = "Draw";
+	} else {
+		generateAlert("success", `Player ${player} has won!`);
+		currentPlayer.innerHTML = `Player ${player} won!`;
+	}
+	return gameBoard.removeEventListener("click", playGame);
+}
+
 function playAgain() {
 	window.location.reload();
 }
@@ -139,12 +154,15 @@ function switchPlayer() {
 		case "X":
 			xMoves++;
 			player = "0";
+			currentPlayer.innerHTML = "It's 0's turn";
 			break;
 		case "0":
 			zeroMoves++;
 			player = "X";
+			currentPlayer.innerHTML = "It's X's turn";
 			break;
 		default:
 			player = "X";
+			currentPlayer.innerHTML = "It's X's turn";
 	}
 }
